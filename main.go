beea05babaacac69368390412df14310e614e711
Billy <billy>
2019-12-10 11:54:43 +0800

    first

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "./routers"
+	"github.com/astaxie/beego"
+	"os"
+	"strconv"
+)
+
+func main() {
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err == nil {
+		beego.BConfig.Listen.HTTPPort = port
+	}
+	beego.Run()
+}
